e25be9eae3149164e9781ee62a93f73560f7e93a
nike935 <nike@growthfactor.ai>
2025-07-31 14:52:15 -0400

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,11 @@
+import { GlobeHemisphereWest } from 'phosphor-react';
+import '../src/index.css';
+
+export default function Header() {
+    return (
+        <header>
+            <GlobeHemisphereWest size={24} color="#ffffff" weight="fill"/>
+            <h1>my travel journey.</h1>
+        </header>
+    )
+}
